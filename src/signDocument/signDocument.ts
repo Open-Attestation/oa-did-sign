f6c66ff0c7041c1b90460cc4ea123b9b958b1d01
SIA CHEN REN <siachenren@gmail.com>
2021-02-18 16:48:25 +0800

    feat: v3 did sign

--- src/signDocument/signDocument.ts
+++ src/signDocument/signDocument.ts
@@ -1,32 +1,53 @@
-import {
-  WrappedDocument,
-  SignedWrappedDocument,
-  ProofType,
-  utils,
-  ProofPurpose,
-  v2,
-} from "@govtechsg/open-attestation";
+import { WrappedDocument, SignedWrappedDocument, utils, v2, v3 } from "@govtechsg/open-attestation";
+
 import { SigningKey } from "src/types";
 import { sign } from "../signer";
 
-export const signDocument = async <T extends v2.OpenAttestationDocument>(
+export const signMerkleRoot = async (algorithm: string, merkleRoot: string, key: SigningKey) =>
+  sign(algorithm, merkleRoot, key);
+
+export enum SUPPORTED_SIGNING_ALGORITHM {
+  Secp256k1VerificationKey2018 = "Secp256k1VerificationKey2018",
+}
+
+export const signDocument = async <T extends v2.OpenAttestationDocument | v3.OpenAttestationDocument>(
   document: SignedWrappedDocument<T> | WrappedDocument<T>,
-  algorithm: string,
+  algorithm: SUPPORTED_SIGNING_ALGORITHM,
   publicKey: string,
   privateKey: string
-): Promise<SignedWrappedDocument<T>> => {
-  if (!utils.isWrappedV2Document(document)) throw new Error("Only v2 document is supported now");
-  const merkleRoot = `0x${document.signature.merkleRoot}`;
+): Promise<v2.OpenAttestationDocument ? v2.SignedWrappedDocument<T> : T extends v3.OpenAttestationDocument ? v3.SignedWrappedDocument<T> : unknown> => {
+  if (!utils.isWrappedV2Document(document) && !utils.isWrappedV3Document(document))
+    throw new Error("Only v2 & v3 document is supported now");
+
+  const merkleRoot = utils.isWrappedV3Document(document)
+    ? `0x${document.proof.merkleRoot}`
+    : `0x${document.signature.merkleRoot}`;
   const signingKey: SigningKey = { private: privateKey, public: publicKey };
-  const signature = await sign(algorithm, merkleRoot, signingKey);
-  const proof = {
-    type: ProofType.OpenAttestationSignature2018,
-    created: new Date().toISOString(),
-    proofPurpose: ProofPurpose.AssertionMethod,
-    verificationMethod: publicKey,
-    signature,
-  };
-  return utils.isSignedWrappedV2Document(document)
-    ? { ...document, proof: [...document.proof, proof] }
-    : { ...document, proof: [proof] };
+  const signature = await signMerkleRoot(algorithm, merkleRoot, signingKey);
+
+  if (utils.isWrappedV3Document(document)) {
+    const proof: v3.VerifiableCredentialProofSigned = {
+      ...document.proof,
+      key: publicKey,
+      signature,
+    };
+
+    const signWrapped: v3.SignedWrappedDocument = { ...document, proof };
+    return signWrapped;
+  }
+  throw new Error("Anything");
+
+  // else{
+  //   const proof = {
+  //     type: ProofType.OpenAttestationSignature2018,
+  //     created: new Date().toISOString(),
+  //     proofPurpose: ProofPurpose.AssertionMethod,
+  //     verificationMethod: publicKey,
+  //     signature,
+  //   };
+
+  //   return utils.isSignedWrappedV2Document(document)
+  //     ? { ...document, proof: [...document.proof, proof] }
+  //     : { ...document, proof: [proof] };
+  // }
 };
