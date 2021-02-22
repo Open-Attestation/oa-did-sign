313e4e8aa36cda7236c0d2625ba1cc72913b6f91
Raymond Yeh <ray@geek.sg>
2021-02-22 16:07:05 +0800

    feat: type overload for document signing

--- src/signDocument/signDocument.ts
+++ src/signDocument/signDocument.ts
@@ -1,58 +1,74 @@
-import {
-  WrappedDocument,
-  SignedWrappedDocument,
-  utils,
-  v2,
-  v3,
-  ProofType,
-  ProofPurpose,
-} from "@govtechsg/open-attestation";
+import { utils, v2, v3, ProofType, ProofPurpose } from "@govtechsg/open-attestation";
 import { SigningKey } from "src/types";
 import { sign } from "../signer";
-
-export const signMerkleRoot = async (algorithm: string, merkleRoot: string, key: SigningKey) =>
-  sign(algorithm, merkleRoot, key);
 
 export enum SUPPORTED_SIGNING_ALGORITHM {
   Secp256k1VerificationKey2018 = "Secp256k1VerificationKey2018",
 }
 
-export const signDocument = async <T extends v2.OpenAttestationDocument | v3.OpenAttestationDocument>(
-  document: SignedWrappedDocument<T> | WrappedDocument<T>,
+export const signV2Document = async <T extends v2.OpenAttestationDocument>(
+  document: v2.SignedWrappedDocument<T> | v2.WrappedDocument<T>,
   algorithm: SUPPORTED_SIGNING_ALGORITHM,
   publicKey: string,
   privateKey: string
-): Promise<SignedWrappedDocument<any>> => {
-  if (!utils.isWrappedV2Document(document) && !utils.isWrappedV3Document(document))
-    throw new Error("Only v2 & v3 document is supported now");
-
-  const merkleRoot = utils.isWrappedV3Document(document)
-    ? `0x${document.proof.merkleRoot}`
-    : `0x${document.signature.merkleRoot}`;
+): Promise<v2.SignedWrappedDocument<T>> => {
   const signingKey: SigningKey = { private: privateKey, public: publicKey };
-  const signature = await signMerkleRoot(algorithm, merkleRoot, signingKey);
-
-  if (utils.isWrappedV3Document(document)) {
-    if (utils.isSignedWrappedV3Document(document)) throw new Error("Document has been signed.");
-
-    const proof: v3.VerifiableCredentialProofSigned = {
-      ...document.proof,
-      key: publicKey,
-      signature,
-    };
-    const signWrapped: v3.SignedWrappedDocument = { ...document, proof };
-    return signWrapped;
-  } else {
-    const proof = {
-      type: ProofType.OpenAttestationSignature2018,
-      created: new Date().toISOString(),
-      proofPurpose: ProofPurpose.AssertionMethod,
-      verificationMethod: publicKey,
-      signature,
-    };
-
-    return utils.isSignedWrappedV2Document(document)
-      ? { ...document, proof: [...document.proof, proof] }
-      : { ...document, proof: [proof] };
-  }
+  const merkleRoot = `0x${document.signature.merkleRoot}`;
+  const signature = await sign(algorithm, merkleRoot, signingKey);
+  const proof = {
+    type: ProofType.OpenAttestationSignature2018,
+    created: new Date().toISOString(),
+    proofPurpose: ProofPurpose.AssertionMethod,
+    verificationMethod: publicKey,
+    signature,
+  };
+  return {
+    ...document,
+    proof: utils.isSignedWrappedV2Document(document) ? [...document.proof, proof] : [proof],
+  };
 };
+
+export const signV3Document = async <T extends v3.OpenAttestationDocument>(
+  document: v3.SignedWrappedDocument<T> | v3.WrappedDocument<T>,
+  algorithm: SUPPORTED_SIGNING_ALGORITHM,
+  publicKey: string,
+  privateKey: string
+): Promise<v3.SignedWrappedDocument<T>> => {
+  const signingKey: SigningKey = { private: privateKey, public: publicKey };
+  const merkleRoot = `0x${document.proof.merkleRoot}`;
+  const signature = await sign(algorithm, merkleRoot, signingKey);
+  const proof: v3.VerifiableCredentialProofSigned = {
+    ...document.proof,
+    key: publicKey,
+    signature,
+  };
+  return { ...document, proof };
+};
+
+export async function signDocument<T extends v3.OpenAttestationDocument>(
+  document: v3.SignedWrappedDocument<T> | v3.WrappedDocument<T>,
+  algorithm: SUPPORTED_SIGNING_ALGORITHM,
+  publicKey: string,
+  privateKey: string
+): Promise<v3.SignedWrappedDocument<T>>;
+export async function signDocument<T extends v2.OpenAttestationDocument>(
+  document: v2.SignedWrappedDocument<T> | v2.WrappedDocument<T>,
+  algorithm: SUPPORTED_SIGNING_ALGORITHM,
+  publicKey: string,
+  privateKey: string
+): Promise<v2.SignedWrappedDocument<T>>;
+export async function signDocument(
+  document: any,
+  algorithm: SUPPORTED_SIGNING_ALGORITHM,
+  publicKey: string,
+  privateKey: string
+) {
+  switch (true) {
+    case utils.isWrappedV2Document(document):
+      return signV2Document(document, algorithm, publicKey, privateKey);
+    case utils.isWrappedV3Document(document):
+      return signV3Document(document, algorithm, publicKey, privateKey);
+    default:
+      throw new Error("Unsupported document type: Only OpenAttestation v2 & v3 documents can be signed");
+  }
+}
